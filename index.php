7fa71a59dffcd464b0cc515c760c37301b8a4ccf
jakubkrawczynski <63091863+jakubkrawczynski@users.noreply.github.com>
2020-10-28 14:47:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
-
+<?php
+    echo("<div style='font-size:30px; margin:30px'><b>Jakub Krawczyński nr18</b></div>");
+    echo("<hr />");
+        echo("<div style='font-size:20px; margin:10px'>123</div>");
+?>
